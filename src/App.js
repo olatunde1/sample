beb86f7d1513515128340fa7d1fb7ed259e049f2
Olatunde <idowuolatunde32@gmail.com>
2022-08-21 22:43:32 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { BrowserRouter as Router,Route,Link, Routes} from "react-router-dom"
 import Profile from './components/Profile'
 import "./App.css"
 import Billing from './components/Billing'
 import Payer from './components/Payer'
-import CompletePurchase from './components/CompletePurchase';
 import PurchaseCompleted from './components/PurchaseCompleted'
 
 
-function App()  {
+const App = () =>  {
+const [currentStep,setCurrentStep] = useState(0)
+  const componentDisplay=() =>{
+    switch (currentStep) {
+      case 0: return <Profile
+        setCurrentStep={setCurrentStep}
+        currentStep={currentStep}
+      />    
+      case 1: return <Billing />
+      case 2: return <Payer /> 
+      case 3: return <PurchaseCompleted /> 
+
+      default:
+        break;
+    }
+  }
   return (
-    <Routes>
-      <Route path="/" element={<Profile />} />
-      <Route path="billing" element={<Billing />} />
-      <Route path="payer" element={<Payer />} />
-      <Route path="purchase" element={<PurchaseCompleted />}/>
-    </Routes>
+    <>
+    {componentDisplay()}
+    </>
+    // <Routes>
+    //   <Route path="/" element={<Profile />} />
+    //   <Route path="billing" element={<Billing />} />
+    //   <Route path="payer" element={<Payer />} />
+    //   <Route path="purchase" element={<PurchaseCompleted />}/>
+    // </Routes>
     
   )
   
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,61 @@
 import React from 'react'
-import CompletePurchase from './CompletePurchase'
+import { Link,NavLink } from 'react-router-dom';
 
 
 
-
-const profile = () => {
+const PersonalInfo = ({ setCurrentStep,  currentStep }) => {
   return (
-    <div>
-        <CompletePurchase />
-       
-    </div>
+    <div className='container'>
+        <div className='app-wrapper'>
+            <div>
+                <h2 className='title'>Complete your Purchase</h2>
+            </div>
+            <div className='Nav'>
+                <p className='NavP active'>Personal Info</p>
+                <p className='NavP'>Billing Info</p>
+                <p className='NavP'>Confirm Payment</p>
+            </div>
+            <form className='form-wrapper'>
+                <div className='name'>
+                    <label className='label'>Name</label>
+                    <input className='input' placeholder='Name' type="text" name='name' />
+                </div>
+                <div className='email'>
+                    <label className='label'>Email Address</label>
+                    <input className='input' placeholder='Email' type="email" name='emailaddress' />
+                </div>
+                <div className='address1'>
+                    <label className='label'>Address 1</label>
+                    <input className='input' placeholder='Address 1' type="text" name='address1' />
+                </div>
+                <div className='address2'>
+                    <label className='label'>Address 2</label>
+                    <input className='input' placeholder='Address 2' type="text" name='address2'/>
+                </div>
+                <div className='venue'>
+                    <div className='local'>
+                        <label className='label'>local Government</label>
+                        <input className='localgovernment' placeholder='local government' type="text" name='localgovernment' />
+                    </div>
+                    <div className='state'>
+                        <label className='label'>State</label>
+                        <select className='stateDetails'>
+                            <option value="abia">Abia</option>
+                            <option value="umaya">umayah</option>
+                            <option value="adamawa">Adamawa</option>
+                            <option value="yola">Yola</option>
+                        </select>
+                    </div>
+                </div>
+                <div>
+                <button className='next' onClick={() => setCurrentStep(currentStep + 1)}>Next</button>
+                    <button className='cancelpayment'>Cancel Payment</button>
+                </div>
 
-   
-  )
+            </form>
+        </div>
+    </div>
+  );
 }
 
-export default profile
+export default PersonalInfo
